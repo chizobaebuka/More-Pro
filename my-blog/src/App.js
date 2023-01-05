0e5cffc6a57f29af30caea3a27fd4bbd726064e3
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-01-05 21:33:50 +0100

    added Home and Navbar component and imported it to the App file component also added some styling

---
+++ my-blog/src/App.js
@@ -0,0 +1,16 @@
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+// You export your component created so it can be used elsewhere on another file
+export default App;
