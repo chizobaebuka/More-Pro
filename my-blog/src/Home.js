a33ebb5f78c0586d6ff450438851a4b5af488e71
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-01-05 22:22:33 +0100

    understanding the use of the useState hook and its functionality and reactiveness to your code/function

--- my-blog/src/Home.js
+++ my-blog/src/Home.js
@@ -1,7 +1,34 @@
+// the useState is used to make any value reactive and has two parameters
+import { useState } from "react";
+
 const Home = () => {
+    // let name = 'mario';
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+
+
+    const handleClick = () => {
+        // You make use of the useState() hook to be able to make any value reactive.
+        setName('luigi'); 
+        setAge(30);    
+    }
+
+    // const handleClickAgain = (name, e) => {
+    //     console.log('hello ' + name, e.target);
+    // }
+
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p> {name} is {age} years old </p>
+            <button onClick={handleClick}>Click Here</button>
+            
+            {/* onclick you make use of the event to show what event is being carried out on clicking */}
+            {/* A case where by you are trying to pass in an argument and also display it you have to create another function within the onclick so it can be shown */}
+            {/* <button onClick={(e) => {
+                handleClickAgain('mario', e)
+            }}>Click Me Again</button> */}
         </div>
      );
 }
