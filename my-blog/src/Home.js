9953e742645636c74ad294e3a1ee33a15a6db3f3
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-01-05 23:05:03 +0100

    using props to pass data from the home component to the Bloglist component

--- my-blog/src/Home.js
+++ my-blog/src/Home.js
@@ -1,17 +1,18 @@
 // the useState is used to make any value reactive and has two parameters
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-    // let name = 'mario';
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
-
-
-    const handleClick = () => {
-        // You make use of the useState() hook to be able to make any value reactive.
-        setName('luigi'); 
-        setAge(30);    
-    }
+    const [blogs] = useState([
+        {title: 'My New Website', body: 'lorem ipsum...', author: 'yoshi', id: 1},
+        {title: 'Welcome Party!', body: 'lorem ipsum...', author: 'mario', id: 2},
+        {title: 'Web Dev top tips', body: 'lorem ipsum...', author: 'marco', id: 3},
+    ]);
+    // const handleClick = () => {
+    //     // You make use of the useState() hook to be able to make any value reactive.
+    //     setName('luigi'); 
+    //     setAge(30);    
+    // }
 
     // const handleClickAgain = (name, e) => {
     //     console.log('hello ' + name, e.target);
@@ -20,15 +21,7 @@ const Home = () => {
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p> {name} is {age} years old </p>
-            <button onClick={handleClick}>Click Here</button>
-            
-            {/* onclick you make use of the event to show what event is being carried out on clicking */}
-            {/* A case where by you are trying to pass in an argument and also display it you have to create another function within the onclick so it can be shown */}
-            {/* <button onClick={(e) => {
-                handleClickAgain('mario', e)
-            }}>Click Me Again</button> */}
+           <BlogList />
         </div>
      );
 }
